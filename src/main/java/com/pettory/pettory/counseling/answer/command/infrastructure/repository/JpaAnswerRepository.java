87b0da2cff2e2b28da6502ff58d64872e4101c18
ygc1994 <ygc1994@naver.com>
2024-10-24 14:35:08 +0900

    #67 [FEAT] 답변 Jpa 레포지토리 interface 추가 -JpaAnswerRepository

--- src/main/java/com/pettory/pettory/counseling/answer/command/infrastructure/repository/JpaAnswerRepository.java
+++ src/main/java/com/pettory/pettory/counseling/answer/command/infrastructure/repository/JpaAnswerRepository.java
@@ -1,7 +1,9 @@
 package com.pettory.pettory.counseling.answer.command.infrastructure.repository;
 
+
 import com.pettory.pettory.counseling.answer.command.domain.aggregate.Answer;
+import com.pettory.pettory.counseling.answer.command.domain.repository.AnswerRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+public interface JpaAnswerRepository extends AnswerRepository, JpaRepository<Answer, Integer> {
 }
