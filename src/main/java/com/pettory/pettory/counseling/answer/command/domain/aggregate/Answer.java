92a6b70d7dfe343502243727acfff95c5afe9c85
ygc1994 <ygc1994@naver.com>
2024-10-24 12:18:12 +0900

    #67 [FEAT] 답변 엔터티 수정 -Answer

--- src/main/java/com/pettory/pettory/counseling/answer/command/domain/aggregate/Answer.java
+++ src/main/java/com/pettory/pettory/counseling/answer/command/domain/aggregate/Answer.java
@@ -1,54 +1,67 @@
 package com.pettory.pettory.counseling.answer.command.domain.aggregate;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "counseling_answer")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE counseling_answer SET counseling_answer_state = 'DELETE', counseling_answer_delete_datetime = NOW() WHERE counseling_answer_num = ?")
 public class Answer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "counseling_answer_num")
-    private int counselingAnswerNum;
-
+    private Integer counselingAnswerNum;
     @Column(name = "counseling_question_num")
-    private int counselingQuestionNum;
-
+    private Integer counselingQuestionNum;
     @Column(name = "counseling_answer_content")
     private String counselingAnswerContent;
-
-    @Enumerated(EnumType.STRING)
+    @Enumerated(value = EnumType.STRING)
     @Column(name = "counseling_answer_state")
-    private AnswerState counselingAnswerState  = AnswerState.ACTIVE;
-
+    private AnswerStatus counselingAnswerState  = AnswerStatus.ACTIVE;
+    @CreatedDate
     @Column(name = "counseling_answer_insert_datetime")
-    private String counselingAnswerInsertDatetime;
-
+    private LocalDateTime counselingAnswerInsertDatetime;
     @Column(name = "counseling_answer_delete_datetime")
-    private String counselingAnswerDeleteDatetime;
-
+    private LocalDateTime counselingAnswerDeleteDatetime;
+    @LastModifiedDate
     @Column(name = "counseling_answer_update_datetime")
-    private String counselingAnswerUpdateDatetime;
-
+    private LocalDateTime counselingAnswerUpdateDatetime;
     @Column(name = "counseling_answer_reanswer_num")
-    private int counselingAnswerReanswerNum;
+    private Integer counselingAnswerReanswerNum;
+    @Column(name = "counseling_answer_file_url")
+    private String counselingAnswerFileUrl;
 
-    public void modifyCounselingAnswer(String counselingAnswerContent, String counselingAnswerUpdateDatetime) {
+    private Answer(Integer counselingQuestionNum, String counselingAnswerContent, Integer counselingAnswerReanswerNum, String counselingAnswerFileUrl) {
+        this.counselingQuestionNum = counselingQuestionNum;
         this.counselingAnswerContent = counselingAnswerContent;
-        this.counselingAnswerUpdateDatetime = counselingAnswerUpdateDatetime;
+        this.counselingAnswerReanswerNum = counselingAnswerReanswerNum;
+        this.counselingAnswerFileUrl = counselingAnswerFileUrl;
     }
 
-    public void removeCounselingAnswer(AnswerState counselingAnswerState, String counselingAnswerDeleteDatetime) {
+    public static Answer create(Integer counselingQuestionNum, String counselingAnswerContent, Integer counselingAnswerReanswerNum, String counselingAnswerFileUrl) {
+        return new Answer(counselingQuestionNum, counselingAnswerContent, counselingAnswerReanswerNum, counselingAnswerFileUrl);
+    }
+
+    public void changeCounselingAnswerFileUrl(String counselingAnswerFileUrl) {
+        this.counselingAnswerFileUrl = counselingAnswerFileUrl;
+    }
+
+    public void updateAnswerDetails(Integer counselingQuestionNum, String counselingAnswerContent, AnswerStatus counselingAnswerState, Integer counselingAnswerReanswerNum) {
+        this.counselingQuestionNum = counselingQuestionNum;
+        this.counselingAnswerContent = counselingAnswerContent;
         this.counselingAnswerState = counselingAnswerState;
-        this.counselingAnswerDeleteDatetime = counselingAnswerDeleteDatetime;
-    }
-
-    public void createSubAnswer(int counselingAnswerReanswerNum) {
         this.counselingAnswerReanswerNum = counselingAnswerReanswerNum;
     }
-
 }
